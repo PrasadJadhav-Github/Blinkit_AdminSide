3b71a54f88cbe960870ca0d5072570cf9a05bc72
PrasadJadhav-Github <prasadrjadhav11@gmail.com>
2024-06-10 12:58:07 +0530

    category adapter complete and implement

--- app/src/main/java/com/example/blinkit_adminside/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/blinkit_adminside/fragments/HomeFragment.kt
@@ -8,17 +8,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import com.example.blinkit_adminside.R
+import com.example.blinkit_adminside.adapter.CategoriesAdapter
+import com.example.blinkit_adminside.databinding.FragmentHomeBinding
+import com.example.blinkit_adminside.models.Category
+import com.example.blinkit_adminside.object_class.Constant
 
 class HomeFragment : Fragment() {
-
+private  lateinit var  binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding= FragmentHomeBinding.inflate(inflater,container,false)
         setStatusBarColor()
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        setCategories()
+        return binding.root
+    }
+
+    private fun setCategories() {
+
+        val  categoryList = ArrayList<Category>()
+
+        for (i in 0 until  Constant.allProductsCategoryIcon.size){
+            categoryList.add(Category(Constant.allProductsCategory[i],Constant.allProductsCategoryIcon[i]))
+        }
+        binding.recyclerviewCategories.adapter=CategoriesAdapter(categoryList)
     }
 
     private fun setStatusBarColor() {
